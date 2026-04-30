1afe9b035d5f2ae1004c5479924021945f2bf9cd
siempre04 <slepuhinde@gmail.com>
2026-04-30 21:59:07 +0500

    добавление логики ввода и вывода

--- Program.cs
+++ Program.cs
@@ -2,8 +2,29 @@ using System;
 class Program
 {
 static void Main(string[] args)
-    {
-        // VERSION: 1.0.0
-        // Здесь будет логика калькулятора
-    }
+{
+    // VERSION: 1.0.0
+    // Здесь будет логика калькулятора
+
+Console.Write("Введите первое число: ");
+string input1 = Console.ReadLine();
+int num1 = int.Parse(input1);
+
+Console.Write("Введите второе число: ");
+string input2 = Console.ReadLine();
+int num2 = int.Parse(input2);
+
+Console.Write("Выберите операцию (+, -, *): ");
+string operation = Console.ReadLine();
+
+int result = 0;
+
+if (operation == "+")
+result = num1 + num2;
+else if (operation == "-")
+result = num1 - num2;
+else if (operation == "*")
+result = num1 * num2;
+
+Console.WriteLine($"Результат: {result}");
 }
